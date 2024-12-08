4a90481632d91c56394230903ecd082cbc92ba29
0524ashley <112052840+0524ashley@users.noreply.github.com>
2024-12-08 12:10:15 -0800

    Update main to show login first

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
+#include "logindialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    // Show the login dialog first
+    LoginDialog loginDialog;
+    if (loginDialog.exec() == QDialog::Accepted) {
+        // If login is successful, show the main window
+        MainWindow w;
+        w.show();
+        return a.exec();
+    } else {
+        // If login is unsuccessful, close the application
+        return 0;
+    }
 }
